55fdf36f02fe8fb353677055eb1e5526b524fdbe
MGRessers <76809261+MGRessers@users.noreply.github.com>
2021-05-01 12:21:16 +0500

    Add files via upload

--- Email Console app/main.py
+++ Email Console app/main.py
@@ -1,40 +1,58 @@
 import smtplib
-import config
 import json
+import config
 
-with open('data.json', 'r') as d:
- json.load(d)
-
-if(config.EMAIL_ADDRESS and config.PASSWORD):
-    dele = input('Do you want to reset the set email and password (yes or no): ')
-    if(dele == "yes"):
-
-        print("Email and password reset, Restart the program.")
-        config.EMAIL_ADDRESS = ""
-        config.PASSWORD = ""
-
-    subject = input(str("Enter your subject: "))
-    msg = input(str("Enter your message: "))
-    send = input(str("Enter the email of who you are sending this to: "))
-else:
-    
-    config.EMAIL_ADDRESS = input(str("Enter your email address you are using to send an email: "))
-    config.PASSWORD = input(str("Enter the password of your email: "))
-    subject = input(str("Enter your subject: "))
-    msg = input(str("Enter your message: "))
-    send = input(str("Enter the email of who you are sending this to: "))
-
-def send_email(subject, msg):
+def send_email(subject, msg, send):
     try:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
-        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        server.login('bigdream66111@gmail.com' , 'Apple100.youtube')
         message = 'Subject: {}\n\n{}'.format(subject, msg)
-        server.sendmail(config.EMAIL_ADDRESS, config.SENDING_TO, message)
+        server.sendmail('bigdream66111@gmail.com' , send, message)
         server.quit()
         print("Email successfully sent.")
     except:
         print("Email failed to send.")
 
-send_email(subject,msg)
+def main():
+    print("1. Reset Email & Password")
+    print("2. Send E-Mail")
+    print("3. Exit")
+    choice = int(input("Choose Menu :"))
+    if(choice == 1):
+        dele = input('Do you want to reset the set email and password (yes or no): ')
+        if(dele == "yes"):
+
+            print("Email and password reset, Restart the program.")
+            config.EMAIL_ADDRESS = ""
+            config.PASSWORD = ""
+        
+        else:
+
+            subject = input("Enter your subject: ")
+            message = input("Enter your message: ")
+            send = input("Enter email of who you are sending to: ")
+            send_email(subject, message, send)
+
+    
+    elif(choice == 2):
+
+        if(config.EMAIL_ADDRESS and config.PASSWORD):
+            subject = input("Enter your subject: ")
+            message = input("Enter your message: ")
+            send = input("Enter email of who you are sending to: ")
+            send_email(subject, message, send)
+        else:
+            config.EMAIL_ADDRESS = input("Enter your email: ")
+            config.PASSWORD = input("Enter your password: ")
+            main()
+
+    elif(choice == 3):
+        quit()
+
+    else:
+        print("Error : There is no such option!")
+        main()
+main()
+
